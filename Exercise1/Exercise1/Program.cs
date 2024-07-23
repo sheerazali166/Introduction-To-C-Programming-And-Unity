55b6213be98dab69914680011136a3a244d787b1
Software Scientist <sheerazali656@gmail.com>
2024-07-23 12:23:41 +0500

    initial commit

---
+++ Exercise1/Exercise1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Exercise1
+{
+
+    /// <summary>
+    /// Solution for Exercise 1
+    /// </summary>
+    class MainClass
+    {
+        /// <summary>
+        /// Prints names
+        /// </summary>
+        /// <param name="args">command-line args</param>
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Aiza Anwar");
+
+            // Problem 1: Output name
+            Console.WriteLine("My name: Dr. T");
+            Console.WriteLine();
+
+            // Problem 2: output best name or namesis
+            Console.WriteLine("My namesis: Administrators");
+
+        }
+    }
+}
