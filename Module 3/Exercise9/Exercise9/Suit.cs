5173b4ccd16880fdcafb05e3d1f5137ae1f062b7
Software Scientist <sheerazali656@gmail.com>
2024-07-23 16:57:03 +0500

    initial commit

---
+++ Module 3/Exercise9/Exercise9/Suit.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Exercise9
+{
+    /// <summary>
+    /// an enumeration for card suits
+    /// </summary>
+
+    public enum Suit {
+
+        Clubs,
+        Diamonds,
+        Hearts,
+        Spades
+    }
+
+}
