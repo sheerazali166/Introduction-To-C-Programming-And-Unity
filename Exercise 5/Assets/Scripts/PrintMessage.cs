c1f9920075d63dffa007268ff385af7ff685b65b
Software Scientist <sheerazali656@gmail.com>
2024-07-23 13:06:32 +0500

    initial commit

---
+++ Exercise 5/Assets/Scripts/PrintMessage.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrintMessage : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello noob!");
+
+        print("DiRT Rally");
+        print("Assetto Corsa");
+        print("Project CARS");
+
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
