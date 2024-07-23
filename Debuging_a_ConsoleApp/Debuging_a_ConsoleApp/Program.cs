ca6cbf156263e2cf85b10bd57961d7c247b9d5ba
Software Scientist <sheerazali656@gmail.com>
2024-07-23 12:24:44 +0500

    initial commit

---
+++ Debuging_a_ConsoleApp/Debuging_a_ConsoleApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Debuging_a_ConsoleApp
+{
+    /// <summary>
+    /// Demonstrate debugging a console app
+    /// </summary>
+
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            float angle;
+
+            // Prompt for and get angle
+            Console.WriteLine("Enter angle in degrees: ");
+            angle = float.Parse(Console.ReadLine());
+
+            // convert angle to radians
+            // angle *= (float)180 / (float)Math.PI;
+            angle *= (float)Math.PI / 180;
+
+            // calculate and print cos and sin
+            Console.WriteLine(Math.Cos(angle));
+            Console.WriteLine(Math.Sin(angle));
+
+        }
+    }
+}
